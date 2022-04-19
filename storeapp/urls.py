a54baee6c56457f42ed1e75a79b34c4c05ba872b
KEEMSY <phzzz@naver.com>
2022-04-19 19:41:32 +0900

    remove apps

--- storeapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from django.views.generic import TemplateView
-
-app_nmae='storeapp'
-
-urlpatterns = [
-    path('list/', TemplateView.as_view(template_name='storeapp:list.html'), name='list')
-]
