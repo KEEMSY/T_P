5d2075d7d8a8d5b2ff47b48c42ae19c3d5e8de4e
zhqmfkv <zhqmfkv@naver.com>
2022-06-01 20:55:03 +0900

    테스트코드 가독성 좋게 수정

--- articleapp/test/service/test_class_method/test_DevelopTeam_class.py
+++ articleapp/test/service/test_class_method/test_DevelopTeam_class.py
@@ -8,7 +8,16 @@ from articleapp.classes.Supporter_class import Supporter
 
 
 class TestDevelopTeamClass(TestCase):
-    # 개발팀 만들기 List 로 주어질 떄
+    """
+        * make
+    """
+
+    """
+    ------------------------------------------------------------------------------------------------------------------
+                                                    make_team(data)
+    ------------------------------------------------------------------------------------------------------------------
+    """
+    # List 로 주어질 떄
     def test_develop_make_team_given_list(self):
         # Given
         m1 = Developer()
@@ -23,7 +32,7 @@ class TestDevelopTeamClass(TestCase):
         # Then
         self.assertEqual(team_list, team.get_team_list())
 
-    # 개발팀 만들기 - 단일 유저로 주어질 때
+    # 단일 유저로 주어질 때
     def test_develop_make_team_given_string(self):
         # Given
         m1 = Developer()
@@ -35,7 +44,7 @@ class TestDevelopTeamClass(TestCase):
         # Then
         self.assertEqual([m1], team.get_team_list())
 
-    # 개발팀 만들기 - 다른 클래스가 들어온다면??
+    # 다른 클래스가 들어온다면??
     def test_develop_make_team_when_other_type_come(self):
         # Given
         team_list = ['m1', 'm2', 'm3']
@@ -46,7 +55,7 @@ class TestDevelopTeamClass(TestCase):
         with self.assertRaises(TypeIsNotDeveloper):
             team.make_team(team_list)
 
-    # 개발팀 만들기 - 리스트 멤버 중 다른 클래스가 있다면?
+    # 리스트 멤버 중 다른 클래스가 있다면?
     def test_developer_make_team_when_data_is_list_and_other_class_into_data(self):
         # Given
         m1 = Developer()
@@ -61,8 +70,14 @@ class TestDevelopTeamClass(TestCase):
         with self.assertRaises(TypeIsNotDeveloper):
             result = team.make_team(team_list)
 
-    # 개발팀 멤버 추가 - 팀 리스트에 멤버가 제대로 추가 되었는가?
-    def test_develop_team_add_member(self):
+    """
+    ------------------------------------------------------------------------------------------------------------------
+                                                add_developer(data)
+    ------------------------------------------------------------------------------------------------------------------
+    """
+
+    # 팀 리스트에 멤버가 제대로 추가 되었는가?
+    def test_develop_team_add_developer(self):
         # Given
         m1 = Developer()
         m2 = Developer()
@@ -81,7 +96,7 @@ class TestDevelopTeamClass(TestCase):
         self.assertTrue(result)
         self.assertEqual(team_list, team.get_team_list())
 
-    # 개발팀 멤버 추가 - 이미 있는 유저가 추가 된다면?
+    # 이미 있는 유저가 추가 된다면?
     def test_develop_team_append_member_when_already_member_exist(self):
         # Given
         m1 = Developer()
@@ -95,7 +110,13 @@ class TestDevelopTeamClass(TestCase):
             # When
             team.append_developer(m1)
 
-    # 개발팀 멤버 제거 - 리스트에서 멤버가 제대로 제거 되는가?
+    """
+    ------------------------------------------------------------------------------------------------------------------
+                                                delete_developer(data)
+    ------------------------------------------------------------------------------------------------------------------
+    """
+
+    # 리스트에서 멤버가 제대로 제거 되는가?
     def test_develop_team_delete_member(self):
         # Given
         m1 = Developer()
@@ -113,7 +134,7 @@ class TestDevelopTeamClass(TestCase):
         self.assertTrue(result)
         self.assertEqual(team_list, team.get_team_list())
 
-    # 개발팀 멤버 제거 - 팀 리스트에서 없는 멤버를 제거할 경우
+    # 팀 리스트에서 없는 멤버를 제거할 경우
     def test_develop_team_delete_member_when_member_does_not_exist(self):
         # Given
         m1 = Developer()
@@ -130,7 +151,13 @@ class TestDevelopTeamClass(TestCase):
         with self.assertRaises(MemberDoesNotExist):
             result = team.delete_developer(m4)
 
-    # 개발팀 리더 설정 - 리더 설정이 제대로 되는가?
+    """
+    ------------------------------------------------------------------------------------------------------------------
+                                                register_leader(data)
+    ------------------------------------------------------------------------------------------------------------------
+    """
+
+    # 리더 설정이 제대로 되는가?
     def test_develop_team_register_leader(self):
         # Given
         m1 = Developer()
@@ -147,7 +174,7 @@ class TestDevelopTeamClass(TestCase):
         self.assertTrue(result)
         self.assertEqual(team.get_leader(), m1)
 
-    # 개발팀 리더 변경 - 리더 변경이 제대로 되는가?
+    # 리더 변경이 제대로 되는가?
     def test_develop_team_change_leader(self):
         # Given
         m1 = Developer()
@@ -165,7 +192,7 @@ class TestDevelopTeamClass(TestCase):
         self.assertTrue(result)
         self.assertEqual(m2, team.get_leader())
 
-    # 개발팀 리더 변경 - data가 developer 객체가 아니라면?
+    # data가 developer 객체가 아니라면?
     def test_develop_team_set_leader_when_data_type_is_not_developer(self):
         # Given
         m1 = Developer()
@@ -180,7 +207,7 @@ class TestDevelopTeamClass(TestCase):
             # When
             result = team.register_leader('test_leader!')
 
-    # 개발팀 리더 변경 - data 가 팀 리스트에 없다면?
+    # data 가 팀 리스트에 없다면?
     def test_develop_team_register_leader_when_data_not_in_team_list(self):
         # Given
         m1 = Developer()
@@ -196,7 +223,13 @@ class TestDevelopTeamClass(TestCase):
             m4 = Developer()
             team.register_leader(m4)
 
-    # 개발팀 서포터 등록 - 동작 확인
+    """
+    ------------------------------------------------------------------------------------------------------------------
+                                                append_supporter(data)
+    ------------------------------------------------------------------------------------------------------------------
+    """
+
+    # 동작 확인
     def test_develop_team_append_supporter(self):
         # Given
         m1 = Developer()
@@ -213,7 +246,7 @@ class TestDevelopTeamClass(TestCase):
         # Then
         self.assertEqual(team.get_supporter_list(), [s1])
 
-    # 개발팀 서포터 등록 - 리스트를 넣었을 때
+    # 리스트를 넣었을 때
     def test_develop_team_append_supporter_append_by_list(self):
         # Given
         m1 = Developer()
@@ -234,7 +267,7 @@ class TestDevelopTeamClass(TestCase):
         # Then
         self.assertEqual(team.get_supporter_list(), [s1, s2, s3, s4])
 
-    # 개발팀 서포터 등록 - 리스트를 넣었을 때 중간에 중복이 있는 경우
+    # 리스트를 넣었을 때 중간에 중복이 있는 경우
     def test_develop_team_append_supporter_append_by_list_and_already_member_exist(self):
         # Given
         team = DevelopTeam()
@@ -249,7 +282,7 @@ class TestDevelopTeamClass(TestCase):
         with self.assertRaises(AlreadyMemberExist):
             team.append_supporter([s2, s1, s3])
 
-    # 개발팀 서포터 등록 - 여러명 동작 확인
+    # 여러명 동작 확인
     def test_develop_team_append_supporter_append_when_supporter_many(self):
         # Given
         m1 = Developer()
@@ -270,7 +303,7 @@ class TestDevelopTeamClass(TestCase):
         # Then
         self.assertEqual(team.get_supporter_list(), [s1, s2, s3])
 
-    # 개발팀 서포터 등록 - 개발자를 등록할 때
+    # 개발자를 등록할 때
     def test_develop_team_append_supporter_when_developer_append_supporter(self):
         # Given
         m1 = Developer()
@@ -286,7 +319,7 @@ class TestDevelopTeamClass(TestCase):
             # When
             team.append_supporter(m4)
 
-    # 개발팀 서포터 등록 - 동일한 유저가 있을 경우
+    # 동일한 유저가 있을 경우
     def test_develop_team_append_supporter_when_develop_already_Exist(self):
         # Given
         m1 = Developer()
@@ -304,7 +337,13 @@ class TestDevelopTeamClass(TestCase):
             # When
             team.append_supporter(s1)
 
-    # 개발팀 서포터 등록 해제 - 동작 확인
+    """
+    ------------------------------------------------------------------------------------------------------------------
+                                                delete_supporter(data)
+    ------------------------------------------------------------------------------------------------------------------
+    """
+
+    # 동작 확인
     def test_develop_team_delete_supporter(self):
         # Given
         m1 = Developer()
@@ -323,7 +362,7 @@ class TestDevelopTeamClass(TestCase):
         # Then
         self.assertEqual(team.get_supporter_list(), [])
 
-    # 개발팀 서포터 등록 해제 - 없는 서포터를 삭제할 경우
+    # 없는 서포터를 삭제할 경우
     def test_develop_team_delete_supporter_when_supporter_is_not_exist(self):
         # Given
         team = DevelopTeam()
